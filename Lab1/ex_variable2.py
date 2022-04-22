0af222ee263ea746509853e3583ef64c50285cb2
Khanittha-bunyang <Khanittha.b@rmutsvmail.com>
2022-04-22 16:30:51 +0700

    test upload to github

---
+++ Lab1/ex_variable2.py
@@ -0,0 +1,18 @@
+# Global variable
+
+x = 'MIT'
+print(x)
+
+def myfunc():
+    # local variable
+    x = 'RUTS'
+    print(x)
+
+def myfunc2():
+    global x
+    x = 'Saiyai'
+    print(x)
+
+# call function()
+myfunc2()
+print(x)
---
+++ Lab10/ex_file2.py
@@ -0,0 +1,19 @@
+# open file with path
+import os
+
+# try:
+#     f = open('C:\\Users\Phatchadawadee\Desktop\lab10.txt')
+#     print(f.read())
+# except Exception as e:
+#     print(e)
+# finally:
+#     f.close()
+
+# delete file
+try:
+    if os.path.exists('C:\\Users\Phatchadawadee\Desktop\lab10.txt'):
+        os.remove('C:\\Users\Phatchadawadee\Desktop\lab10.txt')
+    else:
+        print('File not found.')
+except Exception as e:
+    print(e)
